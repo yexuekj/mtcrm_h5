26e92bf6f15f05125f0193d901e4778a64be78d6
hyp <1539977@qq.com>
2020-10-16 16:26:20 +0800

    '销售简报'

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="index common_page">
         <div class="div_top">
-          <div class="top_left">
+          <div class="top_left" @click="redirect(1)">
             <img class="top_img" src="@/assets/customer_manage.png"/>
             <span class="top_span">客户管理</span>
           </div>
           <div class="top_line">
             <img src="@/assets/line.png" style="height: 174%;width: 31%;"/>
           </div>
-          <div class="top_right">
+          <div class="top_right" @click="redirect(2)">
             <img class="top_img" src="@/assets/customer_sea.png"/>
             <span class="top_span">客户池</span>
           </div>
@@ -16,51 +16,19 @@
       <div class="div_center">
         <h6 style="font-size: 20px;font-weight: 500">销售简报</h6>
         <div class="center_table">
-          <div class="table_list">
+          <div class="table_list" v-for="(item,index) in brief_report" >
             <div class="list_left">
               <img class="list_icon" src="@/assets/icon1.png" />
               <div class="list_font">
-                <span style="color: #25a5ff;float: left">1111</span>
-                <span style="font-size: 15px;float: left">本周新增客户</span>
+                <span style="color: #25a5ff;float: left">{{item[0]}}</span>
+                <span style="font-size: 15px;float: left">本{{item[1]}}新增{{item[4]}}</span>
               </div>
             </div>
             <div class="list_right">
               <img class="list_icon" src="@/assets/icon1.png" />
               <div class="list_font">
-                <span style="color: #25a5ff;float: left">49666</span>
-                <span style="font-size: 15px;float: left">本周新增客户</span>
-              </div>
-            </div>
-          </div>
-          <div class="table_list">
-            <div class="list_left">
-              <img class="list_icon" src="@/assets/icon2.png" />
-              <div class="list_font">
-                <span style="color: #25a5ff;float: left">49666</span>
-                <span style="font-size: 15px;float: left">本周新增联系人</span>
-              </div>
-            </div>
-            <div class="list_right">
-              <img class="list_icon" src="@/assets/icon2.png" />
-              <div class="list_font">
-                <span style="color: #25a5ff;float: left">49666</span>
-                <span style="font-size: 15px;float: left">本周新增联系人</span>
-              </div>
-            </div>
-          </div>
-          <div class="table_list" style="border-bottom: none">
-            <div class="list_left">
-              <img class="list_icon" src="@/assets/icon3.png" />
-              <div class="list_font">
-                <span style="color: #25a5ff;float: left">49666</span>
-                <span style="font-size: 15px;float: left">本周新增商机</span>
-              </div>
-            </div>
-            <div class="list_right">
-              <img class="list_icon" src="@/assets/icon3.png" />
-              <div class="list_font">
-                <span style="color: #25a5ff;float: left">49666</span>
-                <span style="font-size: 15px;float: left">本周新增商机</span>
+                <span style="color: #25a5ff;float: left">{{item[2]}}</span>
+                <span style="font-size: 15px;float: left">本{{item[3]}}新增{{item[4]}}</span>
               </div>
             </div>
           </div>
@@ -75,14 +43,24 @@ export default {
     name: "index",
     data() {
         return {
-          brief_report:[]
+          brief_report:[],
+          userInfo:''
         };
     },
     created() {
         this.$store.state.navbar_title = "CRM"; //导航栏命名
-        this.getIndexH5()
+        this.userInfo = $core.getLocal('loginUserInfo');
+        // this.getIndexH5()
+        this.briefReportInfo()
     },
     methods: {
+      // 跳转
+      redirect(type){
+         switch (type){
+           case 1:
+             this.$router.replace({name: 'customer_list'});
+         }
+      },
         getIndexH5() {
             $core.request("m=index&a=getIndexH5", res => {
                 if (res.status === 1 ) {
@@ -94,25 +72,22 @@ export default {
                 }
             });
         },
-        getMemberList() {
-            //获取会员列表
-            let param = {
-                keywords: this.keywords,
-                user_type_id: this.user_type_id,
-                is_my_serve: this.is_my_serve,
-                page: 1,
-                page_size: 10
-            };
-            $core.request(
-                "api_staff/user/userList",
-                res => {
-                    if (res.code) {
-                        this.member_list = res.data.data;
-                    }
-                },
-                param
-            );
-        }
+       // 销售简报
+       briefReportInfo(){
+        $core.request("m=index&a=index", res => {
+          if (res.status === 1 ) {
+            this.brief_report =[
+              {0:res.data.customer_count.week,1:'周',2:res.data.customer_count.month,3:'月',4:'客户'},
+              {0:res.data.contacts_count.week,1:'周',2:res.data.contacts_count.month,3:'月',4:'联系人'},
+              {0:res.data.business_count.week,1:'周',2:res.data.business_count.month,3:'月',4:'商机'},
+            ];
+            console.log(this.brief_report)
+          } else {
+            Toast.fail(res.info);
+            // this.$router.replace({name: 'Login'});
+          }
+        },{token:this.userInfo.token});
+      }
     }
 };
 </script>
